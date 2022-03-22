f7d4d0f20f9f6335572c85e845992d96c87fb9dd
iDoNtCaRe-69 <59225664+iDoNtCaRe-69@users.noreply.github.com>
2022-03-22 20:29:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+def fizz_buzz(input):
+    if input % 15 == 0:
+        return "FizzBuzz"
+    if input % 3 == 0:
+        return "Buzz"
+    if input % 5 == 0:
+        return "Buzz"
+    return input
+
+
+print(fizz_buzz(10))
